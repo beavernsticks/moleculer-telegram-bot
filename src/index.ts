ffcbd05075c0272da4c333065dba8d9eb37b00b8
Yuriy A Taranov <me@flanker72.ru>
2023-08-28 18:01:55 +0500

    feat: try to use TS properly

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,41 @@
 import { Errors } from 'moleculer'
-import type { TelegramBotServiceSchema } from "./types";
-import { Bot } from "grammy";
+import type { TelegramBotServiceSchema, TelegramBotThis } from "./types";
+import { Bot, RawApi } from "grammy";
+import { Other } from 'grammy/out/core/api';
 
 export const TelegramBotMixin: TelegramBotServiceSchema = {
   name: "TelegramBotMixin",
 
   settings: {
     telegram: {
-      token: process.env.TELEGRAM_BOT_TOKEN || "",
+      token: "",
     },
   },
 
-  methods: {},
+  methods: {
+    async sendMessage(tgId: number, message: string, props: Other<RawApi, "sendMessage">) {
+      this.bot.api.sendMessage(tgId, message, props)
+    }
+  },
 
   created() {
-    if (!this.settings.telegram.token) {
-      throw new Errors.ServiceSchemaError("'telegram.token' is required.", {})
+    const _this = this as unknown as TelegramBotThis;
+    if (!_this.settings.telegram.token) {
+      throw new Errors.ServiceSchemaError("'telegram.token' is required.", {});
     }
-    this.bot = new Bot(this.settings.telegram.token);
-    this.logger.info("Bot created.");
+    _this.bot = new Bot(_this.settings.telegram.token);
+    _this.logger.info("Bot created.");
   },
 
   async started() {
-    await this.bot.start()
-    this.logger.info("Bot started.");
+    const _this = this as unknown as TelegramBotThis;
+    await _this.bot.start();
+    _this.logger.info("Bot started.");
   },
 
   async stopped() {
-    await this.bot.stop()
-    this.logger.info("Bot stopped.");
+    const _this = this as unknown as TelegramBotThis;
+    await _this.bot.stop();
+    _this.logger.info("Bot stopped.");
   }
 };
--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,36 @@
-import { Bot } from "grammy";
-import type { ServiceSchema, ServiceSettingSchema } from "moleculer";
+import { type Bot } from "grammy";
+import { type InputMessageContent } from "grammy/types";
+import type { Service, ServiceSchema, ServiceSettingSchema } from "moleculer";
 
 export interface TelegramBotSettings {
-  /**
-   * Bot token from @BotFather.
-   */
-  token: string;
+  telegram: {
+    /**
+     * Bot token from @BotFather.
+     */
+    token: string;
+  };
 }
 
-type _TelegramBotServiceSchema = ServiceSchema<{
-  telegram: TelegramBotSettings;
-}> &
+type _TelegramBotServiceSchema = ServiceSchema<TelegramBotSettings> &
   ServiceSchema<ServiceSettingSchema>;
 
-export type ServiceMethods = {} & ThisType<_TelegramBotServiceSchema>;
+type _TelegramBotServiceMethods = {
+  sendMessage: (message: string, props: InputMessageContent) => Promise<void>;
+};
+
+type _TelegramBotLocalVariables = {
+  bot: Bot;
+};
+
+export type TelegramBotThis = Service<TelegramBotSettings> &
+  _TelegramBotServiceMethods &
+  _TelegramBotLocalVariables;
+
+export type ServiceMethods = ThisType<
+  _TelegramBotServiceSchema &
+    _TelegramBotServiceMethods &
+    _TelegramBotLocalVariables
+>;
 
 export type TelegramBotServiceSchema = {
   name: "TelegramBotMixin";
